b23ba85268c84f1887169fff320433b5e2a03571
buwen <2543916219@qq.com>
2020-12-16 16:45:34 +0800

    第一次提交github

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @ProjectName: helloworld
+ * @Package: PACKAGE_NAME
+ * @Description: java类作用描述
+ * @Author: 丁鹏飞
+ * @CreateDate: 2020/12/16 16:35
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
