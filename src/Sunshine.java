96109530e4f7daef25f527502f973457d0d6ba52
buwen <2543916219@qq.com>
2020-12-16 17:04:56 +0800

    第二次提交github

---
+++ src/Sunshine.java
@@ -0,0 +1,17 @@
+/**
+ * @ProjectName: hello-world2
+ * @Package: PACKAGE_NAME
+ * @Description: java类作用描述
+ * @Author: 丁鹏飞
+ * @CreateDate: 2020/12/16 17:03
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Sunshine {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sunshine");
+    }
+}
